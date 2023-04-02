09d0bd3d836d391a6576f4c6b1691bf48ea481a8
seoyoung927 <seoyoung0000@naver.com>
2023-04-02 12:03:28 +0900

    edit: Router-StudyFormPage

--- src/Router.jsx
+++ src/Router.jsx
@@ -3,6 +3,7 @@ import App from "./App";
 import LoginPage from "./pages/LoginPage";
 import JoinPage from "./pages/JoinPage";
 import StudiesPage from "./pages/StudiesPage";
+import StudyForm from "./pages/StudyFormPage";
 
 const router = createBrowserRouter([
     {
@@ -23,7 +24,20 @@ const router = createBrowserRouter([
             },
             {
                 path: "community",
-                element: <StudiesPage />
+                children:[
+                    {
+                        path: "",
+                        element: <StudiesPage />,
+                    },
+                    {
+                        path: "studies",
+                        element: <StudiesPage />
+                    },
+                    {
+                        path: "studies/add",
+                        element: <StudyForm />
+                    }
+                ]
             },
         ]
     }
