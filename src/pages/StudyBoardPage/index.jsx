6bca8b8ec21dbd7cfcceda8c652b27aebf8701e2
seoyoung927 <seoyoung0000@naver.com>
2023-04-30 01:58:51 +0900

    edit: StudyBoardPage

--- src/pages/StudyBoardPage/index.jsx
+++ src/pages/StudyBoardPage/index.jsx
@@ -1,11 +1,32 @@
+import { useState, useEffect } from "react";
 import { TiChevronLeft } from "react-icons/ti";
 import { Link, useParams } from "react-router-dom";
 import styles from "./StudyBoardPage.module.css";
 
 function StudyBoard() {
     const params = useParams();
+    const [posts, setPosts] = useState();
+    const getPosts = async (studyId) => {
+        const response = await fetch(`http://localhost:8080/posting/${params.studyId}`, {
+            method: 'GET',
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/json',
+            }
+        });
+        if (!response.ok) throw new Error('bad server condition');
+        return response.json();
+    }
+    useEffect(() => {
+        getPosts(params.studyId).then(res => {
+            setPosts(res.content);
+        });
+    }, []);
 
-    return <>
+    console.log(posts);
+
+    return posts && <>
         <div className={styles.container}>
             <div className={styles.banner__wrapper}>
                 <Link to={`${process.env.PUBLIC_URL}/community/study/${params.studyId}`}><button className={styles.banner__btn}><TiChevronLeft /> 스터디 홈으로</button></Link>
@@ -14,7 +35,7 @@ function StudyBoard() {
                 </div>
             </div>
             <div className={styles.btn__add__wrapper}>
-                <Link to={`${process.env.PUBLIC_URL}/community/study/1/board/add`}>
+                <Link to={`${process.env.PUBLIC_URL}/community/study/${params.studyId}/board/add`}>
                     <button className={styles.btn__add}>
                         <div className={styles.btn__back} />
                         게시물 작성
@@ -22,10 +43,11 @@ function StudyBoard() {
                 </Link>
             </div>
             <div className={styles.board__wrapper}>
-                <div className={styles.board__item}>1주차 공지</div>
-                <div className={styles.board__item}>2주차 공지</div>
-                <div className={styles.board__item}>3주차 공지</div>
-                <div className={styles.board__item}>4주차 공지</div>
+                {posts.map((post) =>
+                    <div
+                        key={post.id}
+                        className={styles.board__item}>{post.title}</div>
+                )}
             </div>
         </div>
     </>
